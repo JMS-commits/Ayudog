0b8f3d1109cb6c7d99e4971435ae16b0cb32e6f1
juandres <juanbucio303@gmail.com>
2020-10-28 22:57:58 -0600

    ordenamiento de las tablas

--- js/intern/data.js
+++ js/intern/data.js
@@ -1,54 +1,59 @@
 firebase.initializeApp(getInit());
-
-function readWalkData2() {
-  var cad = "";
-  firebase.database().ref('/walk/').once('value').then(async function(snapshot) {
-    var cont = 0
-    var arr = []
-    for(var key in snapshot.val()){
-      arr.push(key)
-    }
-    arr.reverse()
-      for(var key in arr){
-          var item = snapshot.val()[arr[key]];
-          var image;
-          var ref = firebase.storage().ref("walk/");
-          var tangRef = ref.child(item.fileName);
-          var blank = item.descripcionWalk!=null?item.descripcionWalk:"";
-          var padding = 'style="padding: 0rem;"'
-          var hide = "d-none"
-          if (item.descripcionWalk!=null && item.descripcionWalk.trim()!="") {
-            padding = 'style="max-height: 100px; overflow-y: auto; background:#5C5C5C;"'
-            hide = ""
-          }
-          image = await tangRef.getDownloadURL();
-          var con = cont>=9 ? "maxElementsWalk":""
-          cad+= '<div class=" col-xs-12 col-sm-12 col-md-4 '+con+' mt-4">'+
-          '<div class="card card-cascade">'+
-            '<div class="view view-cascade" style="background:#5C5C5C;">'
-              if (item.type == "mp4") {
-                cad+= '<iframe style="height: 270px; object-fit: cover; width: 100%; border-radius:5px;" class="card-img-top" src="'+image+'" frameborder="0" allowfullscreen></iframe></iframe>'
-              }else{
-                cad+= '<img src="'+image+'" style="height: 270px; object-fit: cover; width: 100%; border-radius:5px;" class="card-img-top" alt="narrower">'
+var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+          function readWalkData2() {
+            var cad = "";
+            firebase.database().ref('/walk/').once('value').then(async function(snapshot) {
+              var cont = 0
+              var arr = []
+              for(var key in snapshot.val()){
+                arr.push(key)
               }
-              cad+='<a><div class="mask rgba-white-slight"></div></a>'+
-            '</div>'+
-            '<div class="card-body card-body-cascade text-center" '+padding+'>'+
-              '<a class="text-white showData '+hide+'" id="arrow-down-'+key+'" data-id="'+key+'"><i class="fas fa-chevron-down"></i></a>'+
-              '<a class="text-white hideData d-none" id="arrow-'+key+'" data-id="'+key+'"><i class="fas fa-chevron-up"></i></a>'+
-              '<p class="card-text text-white d-none" id="data-'+key+'">'+blank+'</p>'+
-            '</div></div>'
-          
-          cad+= '</div>';
-          cont++
-      }
-      if (cont <= 9) {
-        $("#groupButtonWalk").addClass("d-none")
-      }
-      $("#walkRender").html(cad);
+              arr.reverse()
+                for(var key in arr){
+                    var item = snapshot.val()[arr[key]];
+                    var image;
+                    var ref = firebase.storage().ref("walk/");
+                    var tangRef = ref.child(item.fileName);
+                    var blank = item.descripcionWalk!=null?item.descripcionWalk:"";
+                    var padding = 'style="padding: 0rem;"'
+                    var hide = "d-none"
+                    if (item.descripcionWalk!=null && item.descripcionWalk.trim()!="") {
+                      padding = 'style="max-height: 100px; overflow-y: auto; background:#5C5C5C;"'
+                      hide = ""
+                    }
+                    var number = 9
+                    
+                    if (/android/i.test(userAgent) || (/iPhone|iPod/.test(userAgent) && !window.MSStream) || /windows phone/i.test(userAgent)) {
+                      number = 3   
+                    }
+                    image = await tangRef.getDownloadURL();
+                    var con = cont>=number ? "maxElementsWalk":""
+                    cad+= '<div class=" col-xs-12 col-sm-12 col-md-4 '+con+' mt-4">'+
+                    '<div class="card card-cascade">'+
+                      '<div class="view view-cascade" style="background:#5C5C5C;">'
+                        if (item.type == "mp4") {
+                          cad+= '<iframe style="height: 270px; object-fit: cover; width: 100%; border-radius:5px;" class="card-img-top" src="'+image+'" frameborder="0" allowfullscreen></iframe>'
+                        }else{
+                          cad+= '<img src="'+image+'" style="height: 270px; object-fit: cover; width: 100%; border-radius:5px;" class="card-img-top" alt="narrower">'
+                        }
+                        cad+='<a><div class="mask rgba-white-slight"></div></a>'+
+                      '</div>'+
+                      '<div class="card-body card-body-cascade text-center" '+padding+'>'+
+                        '<a class="text-white showData '+hide+'" id="arrow-down-'+key+'" data-id="'+key+'"><i class="fas fa-chevron-down"></i></a>'+
+                        '<a class="text-white hideData d-none" id="arrow-'+key+'" data-id="'+key+'"><i class="fas fa-chevron-up"></i></a>'+
+                        '<p class="card-text text-white d-none" id="data-'+key+'">'+blank+'</p>'+
+                      '</div></div>'
+                    
+                    cad+= '</div>';
+                    cont++
+                }
+                if (cont <= 9) {
+                  $("#groupButtonWalk").addClass("d-none")
+                }
+                $("#walkRender").html(cad);
 
-  });
-}
+            });
+          }
 
           function readWalkData() {
               var cad = "";
@@ -59,6 +64,11 @@ function readWalkData2() {
                   arr.push(key)
                 }
                 arr.reverse()
+                var number = 9
+          
+                if (/android/i.test(userAgent) || (/iPhone|iPod/.test(userAgent) && !window.MSStream) || /windows phone/i.test(userAgent)) {
+                  number = 3   
+                }
                   for(var key in arr){
                       var item = snapshot.val()[arr[key]];
                       var image;
@@ -66,7 +76,7 @@ function readWalkData2() {
                       var tangRef = ref.child(item.fileName);
                       var blank = item.descripcionWalk!=null?item.descripcionWalk:"";
                       image = await tangRef.getDownloadURL();
-                      var con = cont>=9 ? "maxElementsWalk":""
+                      var con = cont>=number ? "maxElementsWalk":""
                       cad+= '<div class=" col-xs-12 col-sm-12 col-md-4 container2 '+con+'">'
                       if (item.type == "mp4") {
                         cad+= '<iframe style="height: 270px; object-fit: cover; width: 100%; border-radius:5px;" class="mt-4" src="'+image+'" frameborder="0" allowfullscreen></iframe></iframe>'
@@ -85,7 +95,7 @@ function readWalkData2() {
                       cad+= '</div>';
                       cont++
                   }
-                  if (cont <= 9) {
+                  if (cont <= number) {
                     $("#groupButtonWalk").addClass("d-none")
                   }
                   $("#walkRender").html(cad);
@@ -101,6 +111,11 @@ function readWalkData2() {
                   arr.push(key)
                 }
                 arr.reverse()
+                var number = 8
+          
+                if (/android/i.test(userAgent) || (/iPhone|iPod/.test(userAgent) && !window.MSStream) || /windows phone/i.test(userAgent)) {
+                  number = 3   
+                }
                   for(var key in arr){
                       var item = snapshot.val()[arr[key]];
                       var image;
@@ -108,7 +123,7 @@ function readWalkData2() {
                       var ref = firebase.storage().ref("adoption/");
                       var tangRef = ref.child(item.fileName);
                       image = await tangRef.getDownloadURL()
-                      var con = cont>=8 ? "maxElements":""
+                      var con = cont>=number ? "maxElements":""
                       cad+= 
                       '<div class="col-md-3 col-sm-12 col-xs-12 '+con+'" style="margin-top: 10px;"><div class="cardS"><div class="image">'+
                         '<img style="background-size: cover;width: 100%;height: 100%;" src="'+image+'"/>'+
@@ -120,7 +135,7 @@ function readWalkData2() {
                               '</div></div></div></div></div>'
                       cont++
                   }
-                  if (cont <= 8) {
+                  if (cont <= number) {
                     $("#groupButtonAdoption").addClass("d-none")
                   }
                   $("#adoptionRender").html(cad);
@@ -135,6 +150,11 @@ function readWalkData2() {
                   arr.push(key)
                 }
                 arr.reverse()
+                var number = 12
+          
+                if (/android/i.test(userAgent) || (/iPhone|iPod/.test(userAgent) && !window.MSStream) || /windows phone/i.test(userAgent)) {
+                  number = 3   
+                }
                   for(var key in arr) {
                     var item = snapshot.val()[arr[key]];
                     var image;
@@ -143,7 +163,7 @@ function readWalkData2() {
                     image = await tangRef.getDownloadURL();
                     var text1 = item.descripcionNews.substring(0, 87);
                     var text2 = item.descripcionNews.substring(87, text1.lenght);
-                    var hideItems = cont>=12 ? "maxElementsNews":""
+                    var hideItems = cont>=number ? "maxElementsNews":""
                     if(item.isPrincipal == 1){
                       cad+= '<div class="col-12 '+hideItems+'" style="margin-top:20px;">'+
                         '<div class="row">'+
@@ -163,7 +183,7 @@ function readWalkData2() {
                     }
                     cont++
                   }
-                  if (cont <= 12) {
+                  if (cont <= number) {
                     $("#groupButtonNews").addClass("d-none")
                   }
                   $("#newsRender").html(cad);
